ccd98685aeb4783e1056e5187fce653be097b7f8
danilo.volles <danilo.volles@outlook.com>
2024-04-08 22:40:57 -0300

    One to one fixings

--- src/main/kotlin/br/com/danilovolles/sistemadecadastrodeestagiarios/entity/Employee.kt
+++ src/main/kotlin/br/com/danilovolles/sistemadecadastrodeestagiarios/entity/Employee.kt
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
 
 @Entity
 data class Employee (
@@ -13,4 +14,15 @@ data class Employee (
 
         val admissionDate: String,
 
+        @OneToOne(mappedBy = "employee")
+        val personalData: PersonalData? = null,
+
+        @OneToOne(mappedBy = "employee")
+        val documentData: DocumentData? = null,
+
+        @OneToOne(mappedBy = "employee")
+        val bankData: BankData? = null,
+
+        @OneToOne(mappedBy = "employee")
+        val academicData: AcademicData? = null
 )
